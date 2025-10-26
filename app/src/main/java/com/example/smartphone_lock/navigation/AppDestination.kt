2937226e26cbb6bff3ca4bb4398fe737d9e44537
yu-yaba <kawai.yuya3401@gmail.com>
2025-10-26 18:24:16 +0900

    feat/MainActivity から NavHost を呼び出すよう更新

---
+++ app/src/main/java/com/example/smartphone_lock/navigation/AppDestination.kt
@@ -0,0 +1,8 @@
+package com.example.smartphone_lock.navigation
+
+enum class AppDestination(val route: String) {
+    Home("home"),
+    LockSetting("lock_setting"),
+    Auth("auth"),
+    Complete("complete");
+}
