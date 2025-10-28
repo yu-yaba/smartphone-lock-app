6d3f78fe9b7ecf2f24728a66b0171936c106492a
yu-yaba <kawai.yuya3401@gmail.com>
2025-10-28 21:58:36 +0900

    feat: MainActivityにConfigリポジトリを注入

--- app/src/main/java/com/example/smartphone_lock/MainActivity.kt
+++ app/src/main/java/com/example/smartphone_lock/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.smartphone_lock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.smartphone_lock.data.repository.ConfigRepository
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.smartphone_lock.ui.theme.SmartphoneLockTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var configRepository: ConfigRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // DI パイプラインの初期化が起動時に行われることを保証する。
+        configRepository.getAppConfig()
         setContent {
             SmartphoneLockTheme {
                 SmartphoneLockApp()
