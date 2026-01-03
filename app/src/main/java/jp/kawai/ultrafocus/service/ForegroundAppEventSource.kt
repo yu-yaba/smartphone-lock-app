f9853851c75750c78cbe210a5007a50ae0fd4391
yu-yaba <kawai.yuya3401@gmail.com>
2026-01-03 16:13:13 +0900

    privacy policy

--- app/src/main/java/jp/kawai/ultrafocus/service/ForegroundAppEventSource.kt
+++
@@ -1,9 +0,0 @@
-package jp.kawai.ultrafocus.service
-
-/**
- * UsageStats などを通して前面アプリイベントを提供するデータソース。
- */
-fun interface ForegroundAppEventSource {
-    @Throws(SecurityException::class)
-    fun collectRecentEvents(windowMillis: Long, onEvent: (String) -> Unit)
-}
